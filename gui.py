b5e56243b97cacffa8166f4f887bcc36e5c3114e
fdre <dr@werkgymnasium.de>
2021-02-05 12:49:35 +0100

    GUI Fileselect

--- gui.py
+++ gui.py
@@ -1,7 +1,7 @@
 import PySimpleGUI as sg
 
 sg.theme("DarkTeal11")
-layout = [[sg.T("")], [sg.Text("Datei: "), sg.Input(key="-IN2-" ,change_submits=True), sg.FileBrowse(key="-IN-")],[sg.Button("Parse") , sg.Button("Exit")]]
+layout = [[sg.T("")], [sg.Text("Datei: "), sg.Input(key="-IN2-" ,change_submits=True), sg.FileBrowse(key="-INPUTFILE-", file_types=(("Etherpad Files", "*.etherpad"),))],[sg.Button("OK") , sg.Button("Exit")]]
 
 ###Building Window
 window = sg.Window('Etherpad-Parser', layout, size=(600,150))
@@ -11,5 +11,13 @@ while True:
     print(values["-IN2-"])
     if event == sg.WIN_CLOSED or event=="Exit":
         break
-    elif event == "Parse":
-        print(values["-IN-"])
+    elif event == "OK":
+        file = values["-INPUTFILE-"]
+        try:
+            file.endswith('.etherpad')
+            print(file)
+        except:
+            sg.Popup('Error', 'Falsches Dateiformat')
+        
+
+        
